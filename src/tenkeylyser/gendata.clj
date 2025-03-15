80cef4be5079c2bd094dc959a44892bc31f22080
5quidwyrm <henochtang09@gmail.com>
2025-03-15 20:30:09 +0000

    Refactored reading/writing ngram data to readwrite.clj

--- src/tenkeylyser/gendata.clj
+++ src/tenkeylyser/gendata.clj
@@ -22,18 +22,3 @@
      :bigrams   (ngrams 2)
      :trigram   (ngrams 3)}))
 
-(defn writedata
-  ; This takes in a map of ngram data, and writes it to a file
-  ; in the ./data/ directory with the same name, and file extension .ngram
-  [data]
-  (assert (not (nil? (data :name))))
-  (let [filename (str "./data/" (data :name) ".ngram")]
-    (->> data (str) (spit filename))))
-
-(defn readdata
-  ; This takes a file name and looks for a file in the data directory.
-  ; Then it reads from that file, and evals that file.
-  ; Assuming all goes well, the file should contain n-gram data
-  ; for the corpora of the name of the file.
-  [corpusname]
-  (->> corpusname (#(str "./data/" % ".ngram")) (slurp) (read-string)))
--- src/tenkeylyser/readwrite.clj
+++ src/tenkeylyser/readwrite.clj
@@ -7,3 +7,20 @@
 
 (defn writelayout [layout]
   (->> layout (str) (spit (str "./layouts/" (layout :name) layoutfileext))))
+
+(defn writedata
+  ; This takes in a map of ngram data, and writes it to a file
+  ; in the ./data/ directory with the same name, and file extension .ngram
+  [data]
+  (assert (not (nil? (data :name))))
+  (let [filename (str "./data/" (data :name) ".ngram")]
+    (->> data (str) (spit filename))))
+
+(defn readdata
+  ; This takes a file name and looks for a file in the data directory.
+  ; Then it reads from that file, and evals that file.
+  ; Assuming all goes well, the file should contain n-gram data
+  ; for the corpora of the name of the file.
+  [corpusname]
+  (->> corpusname (#(str "./data/" % ".ngram")) (slurp) (read-string)))
+
