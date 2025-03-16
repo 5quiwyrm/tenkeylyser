5a2412521dac64fde99b24bbbff99f2902c249e4
5quidwyrm <henochtang09@gmail.com>
2025-03-16 16:22:52 +0000

    Added travel-distance stat calculations

--- src/tenkeylyser/calcstats.clj
+++ src/tenkeylyser/calcstats.clj
@@ -6,15 +6,18 @@
 ; Heat map scores are defined arbitrarily, but the auto-generator treats the heatmap score of a character as the sum of the locations.
 ; The heat map will be a function that takes in a vector of coordinates and spits out the heatmap score.
 
+(defn getloc [character layout]
+  (if (nil? ((layout :left) character))
+    (if (nil? ((layout :right) character))
+      nil
+      ((layout :right) character))
+    ((layout :left) character)))
+
 (defn hmscore [character layout]
   (let [hm {[-2 1]  5.0 [-1 1]  4.0 [0 1]  3.0
             [-2 0]  3.0 [-1 0]  2.0 [0 0]  1.0
             [-2 -1] 4.0 [-1 -1] 3.0 [0 -1] 2.0}
-        locs (if (nil? ((layout :left) character))
-               (if (nil? ((layout :right) character))
-                 nil
-                 ((layout :right) character))
-               ((layout :left) character))]
+        locs (getloc character layout)]
     (reduce +
             (for [l locs] (hm l)))))
 
@@ -25,3 +28,22 @@
             (for [i (keys monograms) f (vals monograms)]
               (* (hmscore (first i) viewlayout) f)))))
 
+(defn bigramdist [bigram layout]
+  (assert (= 2 (count bigram)))
+  (let [loc1 (getloc (first bigram) layout)
+        loc2 (getloc (second bigram) layout)
+        locs (apply conj loc1 (for [i loc2] i))]
+    (reduce + (for [i (range (dec (count locs)))]
+                (let [curr (nth locs i)
+                      next (nth locs (inc i))
+                      sq (fn [x] (* x x))
+                      diffx (sq (- (nth curr 0) (nth next 0)))
+                      diffy (sq (- (nth curr 1) (nth next 1)))]
+                  (Math/sqrt (+ diffx diffy)))))))
+
+(defn totaldist [viewlayout data]
+  (assert (not (nil? (data :bigrams))))
+  (let [bigrams (data :bigrams)]
+    (reduce +
+            (for [i (keys bigrams) f (vals bigrams)]
+              (* (bigramdist i viewlayout) f)))))
