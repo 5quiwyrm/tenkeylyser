4d2da559a7a102ce9cb903007e801838bd6ba07f
5quidwyrm <henochtang09@gmail.com>
2025-03-26 16:51:26 +0000

    Added swipe-tap and repeated swipes stats to default run.

--- src/tenkeylyser/core.clj
+++ src/tenkeylyser/core.clj
@@ -13,11 +13,13 @@
         thms (cs/totalhmscore ex/hyper d)
         tdst (cs/totaldist ex/hyper d)
         talt (cs/totalalt ex/hyper d)
-        tswt (cs/totalswipetap ex/hyper d)]
-    (printf "Total heat map score: %f.2\n" thms)
-    (printf "Average heat map score: %f.2\n" (->> (d :count) (/ thms)))
-    (printf "Total distance score: %f.2\n" tdst)
-    (printf "Average distance score: %f.2\n" (->> (d :count) (dec) (/ talt)))
+        tswt (cs/totalswipetap ex/hyper d)
+        trt  (cs/totalrepswipe ex/hyper d)]
+    (printf "Total heat map score: %.2f\n" thms)
+    (printf "Average heat map score: %.2f\n" (->> (d :count) (/ thms)))
+    (printf "Total distance score: %.2f\n" tdst)
+    (printf "Average distance score: %.2f\n" (->> (d :count) (dec) (/ talt)))
     (printf "Alt percentage: %.2f%%\n" (->> (d :count) (dec) (/ talt) (* 100)))
-    (printf "Swipe-tap / Tap-swipe: %.2f%%\n" (->> (d :count) (dec) (/ tswt) (* 100)))))
+    (printf "Swipe-tap / Tap-swipe: %.2f%%\n" (->> (d :count) (dec) (/ tswt) (* 100)))
+    (printf "Repeated swipes: %.2f%%\n", (->> (d :count) (dec) (/ trt) (* 100)))))
 
