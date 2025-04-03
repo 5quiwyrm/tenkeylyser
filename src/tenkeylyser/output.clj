a7dda7cfb13fe04ffc2620ad1e0e9c6152140563
5quidwyrm <henochtang09@gmail.com>
2025-04-03 14:40:00 +0100

    Changed to use - instead of = in boxes.

--- src/tenkeylyser/output.clj
+++ src/tenkeylyser/output.clj
@@ -55,7 +55,7 @@
         rightout  (filterlocs right out)]
     (do (printf "Layout: %s\n" (viewlayout :name))
         (printf "
-=====================================================
+-------------------------   -------------------------
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
@@ -67,9 +67,9 @@
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
 | %c %c %c | %c %c %c | %c %c %c |   | %c %c %c | %c %c %c | %c %c %c |
-=====================================================
+-------------------------   -------------------------
 
-" 
+"
                 \space (ccoerce (leftup  [-2 1])) \space
                 \space (ccoerce (leftup  [-1 1])) \space
                 \space (ccoerce (leftup  [0  1])) \space
@@ -126,20 +126,20 @@
                 \space (ccoerce (rightdown [0  -1])) \space
                 \space (ccoerce (rightdown [-1 -1])) \space
                 \space (ccoerce (rightdown [-2 -1])) \space)))
-   (let [d    data
-         thms (cs/totalhmscore  viewlayout data)
-         tdst (cs/totaldist     viewlayout data)
-         talt (cs/totalalt      viewlayout data)
-         tswt (cs/totalswipetap viewlayout data)
-         trt  (cs/totalrepswipe viewlayout data)]
-     (printf "Corpus: %s\n" (d :name))
-     (printf "--------------------------------------------\n")
-     (->> thms                              (format "Total heat map score:   %.2f")   (printf "| %-40s |\n"))
-     (->> (d :count) (/ thms)               (format "Average heat map score: %.2f")   (printf "| %-40s |\n"))
-     (->> tdst                              (format "Total distance score:   %.2f")   (printf "| %-40s |\n"))
-     (->> (d :count) (dec) (/ talt)         (format "Average distance score: %.2f")   (printf "| %-40s |\n"))
-     (->> (d :count) (dec) (/ talt) (* 100) (format "Alt percentage:         %.2f%%") (printf "| %-40s |\n"))
-     (->> (d :count) (dec) (/ tswt) (* 100) (format "Swipe-tap / Tap-swipe:  %.2f%%") (printf "| %-40s |\n"))
-     (->> (d :count) (dec) (/ trt)  (* 100) (format "Repeated swipes:        %.2f%%") (printf "| %-40s |\n"))
-     (printf "--------------------------------------------\n")))
+  (let [d    data
+        thms (cs/totalhmscore  viewlayout data)
+        tdst (cs/totaldist     viewlayout data)
+        talt (cs/totalalt      viewlayout data)
+        tswt (cs/totalswipetap viewlayout data)
+        trt  (cs/totalrepswipe viewlayout data)]
+    (printf "Corpus: %s\n" (d :name))
+    (printf "--------------------------------------------\n")
+    (->> thms                              (format "Total heat map score:   %.2f")   (printf "| %-40s |\n"))
+    (->> (d :count) (/ thms)               (format "Average heat map score: %.2f")   (printf "| %-40s |\n"))
+    (->> tdst                              (format "Total distance score:   %.2f")   (printf "| %-40s |\n"))
+    (->> (d :count) (dec) (/ talt)         (format "Average distance score: %.2f")   (printf "| %-40s |\n"))
+    (->> (d :count) (dec) (/ talt) (* 100) (format "Alt percentage:         %.2f%%") (printf "| %-40s |\n"))
+    (->> (d :count) (dec) (/ tswt) (* 100) (format "Swipe-tap / Tap-swipe:  %.2f%%") (printf "| %-40s |\n"))
+    (->> (d :count) (dec) (/ trt)  (* 100) (format "Repeated swipes:        %.2f%%") (printf "| %-40s |\n"))
+    (printf "--------------------------------------------\n")))
 
